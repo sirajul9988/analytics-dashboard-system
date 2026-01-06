3c26c9e0704803c532536c0d49920672c299a4c8
sirajul9988 <161524666+sirajul9988@users.noreply.github.com>
2026-01-06 22:12:46 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function loadData() {
+  document.getElementById("users").innerText = 1240;
+  document.getElementById("sessions").innerText = 312;
+  document.getElementById("revenue").innerText = "$18,450";
+}
